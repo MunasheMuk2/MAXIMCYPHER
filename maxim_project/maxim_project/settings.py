7dd487067ea51944a297a4fb1453e4e9659e427e
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-12 18:26:29 +0100

    Add all auth templates

--- maxim_project/maxim_project/settings.py
+++ maxim_project/maxim_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "maxim_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "maxim_project" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
