7fc34735bd05af4e761dc6db8d10a22abb8bb04d
MunasheMuk2 <munashemukandi@yahoo.co.uk>
2025-10-13 21:28:08 +0100

    Add app to setting and set up views and urls

--- maxim_project/maxim_project/settings.py
+++ maxim_project/maxim_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'allauth.account.middleware.AccountMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "maxim_project.urls"
